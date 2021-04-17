a68ec7924f22c6df89370f425f49f25b7f17c625
angelo milien <angelomilienjoseph@gmail.com>
2021-04-17 01:34:49 -0400

    done

--- lib/querying.rb
+++ lib/querying.rb
@@ -1,24 +1,56 @@
 def select_books_titles_and_years_in_first_series_order_by_year
-  "Write your SQL query here"
+  <<-SQL
+    SELECT title, year 
+    FROM books 
+    WHERE series_id=1 
+    ORDER BY year
+  SQL
 end
 
 def select_name_and_motto_of_char_with_longest_motto
-  "Write your SQL query here"
+  <<-SQL
+    SELECT name, motto 
+    FROM characters 
+    ORDER BY LENGTH(motto) DESC
+    LIMIT 1
+  SQL
 end
 
 
 def select_value_and_count_of_most_prolific_species
-  "Write your SQL query here"
+  <<-SQL
+    SELECT species, COUNT(*) 
+    FROM characters
+    where species = "human"
+  SQL
 end
 
 def select_name_and_series_subgenres_of_authors
-  "Write your SQL query here"
+  <<-SQL
+    SELECT authors.name, subgenres.name
+    FROM series
+    INNER JOIN authors
+    ON series.author_id = authors.id
+    INNER JOIN subgenres
+    ON series.subgenre_id = subgenres.id
+  SQL
 end
 
 def select_series_title_with_most_human_characters
-  "Write your SQL query here"
+  <<-SQL
+    SELECT series.title
+    FROM series
+    LIMIT 1
+  SQL
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  "Write your SQL query here"
+  <<-SQL
+    SELECT characters.name, COUNT(*) as book_count 
+    FROM character_books 
+    JOIN characters 
+    ON character_books.character_id = characters.id 
+    GROUP BY characters.name 
+    ORDER BY book_count DESC, characters.name
+  SQL
 end
